65723c549def40962de9bc536c5d0eb79bf69739
Maksym Medynskyi <maxwhite.nemetc@gmail.com>
2023-11-27 22:01:33 +0200

    VCH-XXX: Add DB migrations

--- backend/src/migrations/1700311796688-ProjectImageLink.ts
+++ backend/src/migrations/1700311796688-ProjectImageLink.ts
@@ -1,15 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class ProjectImageLink1700311796688 implements MigrationInterface {
-    name = 'ProjectImageLink1700311796688'
+export class projectimageLink1700311796688 implements MigrationInterface {
+    name = 'projectimageLink1700311796688'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Image" ADD "projectId" integer`);
-        await queryRunner.query(`ALTER TABLE "Image" ADD CONSTRAINT "FK_63c8a589bd02b3525546ac70795" FOREIGN KEY ("projectId") REFERENCES "Project"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "image" ADD "projectId" integer`);
+        await queryRunner.query(`ALTER TABLE "image" ADD CONSTRAINT "FK_63c8a589bd02b3525546ac70795" FOREIGN KEY ("projectId") REFERENCES "project"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "Image" DROP CONSTRAINT "FK_63c8a589bd02b3525546ac70795"`);
-        await queryRunner.query(`ALTER TABLE "Image" DROP COLUMN "projectId"`);
+        await queryRunner.query(`ALTER TABLE "image" DROP CONSTRAINT "FK_63c8a589bd02b3525546ac70795"`);
+        await queryRunner.query(`ALTER TABLE "image" DROP COLUMN "projectId"`);
     }
 }
